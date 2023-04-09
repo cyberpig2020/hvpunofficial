397c04bbd3351da38fcf9642e7d52367f7c5792c
otimor-redmi <130282935+otimor-redmi@users.noreply.github.com>
2023-04-09 12:16:49 +0200

    Update item.mjs
    Literowka

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -2,7 +2,7 @@
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
  */
-export class HvunofficialItem extends Item {
+export class HvpunofficialItem extends Item {
   /**
    * Augment the basic Item data model with additional dynamic data.
    */
