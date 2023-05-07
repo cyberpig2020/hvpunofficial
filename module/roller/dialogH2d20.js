f3eaf100fe529c04ad5d4c244318548e7bdf29c5
Artur Pyra <artur@pyra.net.pl>
2023-05-07 20:58:55 +0200

    Fix syntax

--- module/roller/dialogH2d20.js
+++ module/roller/dialogH2d20.js
@@ -20,35 +20,44 @@ export class DialogH2d20 extends Dialog {
             let attr = html.find('[name="attribute"]').val();
             let skill = html.find('[name="skill"]').val();
 
-            game.hvpunofficail.HeroicRoller2D20 .rollHd20({
+            game.hvpunofficail.HeroicRoller2D20.rollHd20({
                     rollname: this.rollName,
                     attribute: attr,
                     skill_level: skill,
                     item: this.item,
                     actor: this.actor });
-    }
+        }
+        )
 
-    static async createDialog({ rollName = "Roll Heroic D20", fatum = false, attribute = 0, skill = 0, complication = 15, actor=null, item=null } = {}) {
-        let dialogData = {}
-        dialogData.rollName = rollName;
-        dialogData.attribute = attribute;
-        dialogData.fatum = fatum
-        dialogData.skill = skill;
-        dialogData.complication = complication;
-        dialogData.actor = actor;
-        dialogData.item = item;
-        const html = await renderTemplate("systems/hvpunoficial/templates/dialogs/dialogH2d20.hbs", dialogData);
+    static async createDialog({
+        rollName = "Roll Heroic D20",
+        fatum = false,
+        attribute = 0,
+        skill = 0,
+        complication = 15,
+        actor=null, item=null } = {}) {
 
-        let d = new DialogH2d20(rollName, attribute, skill, fatum, complication, actor, item, {
-            title: rollName,
-            content: html,
-            buttons: {
-                roll: {
-                    icon: '<i class="fas fa-check"></i>',
-                    label: "ROLL"
+            let dialogData = {}
+            dialogData.rollName = rollName;
+            dialogData.attribute = attribute;
+            dialogData.fatum = fatum
+            dialogData.skill = skill;
+            dialogData.complication = complication;
+            dialogData.actor = actor;
+            dialogData.item = item;
+            const html = await renderTemplate("systems/hvpunoficial/templates/dialogs/dialogH2d20.hbs", dialogData);
+
+            let d = new DialogH2d20(rollName, attribute, skill, fatum, complication, actor, item, {
+                title: rollName,
+                content: html,
+                buttons: {
+                    roll: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: "ROLL"
+                    }
                 }
-            }
-        });
-        d.render(true);
+            });
+            d.render(true);
+        }
     }
 }
